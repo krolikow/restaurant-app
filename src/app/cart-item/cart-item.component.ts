189100393ba4f97e9161792334f29756581395f8
krolikow <mkrolikowska01@poczta.fm>
2023-10-09 16:56:49 +0200

    feat: cart item template

--- src/app/cart-item/cart-item.component.ts
+++ src/app/cart-item/cart-item.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
+import {Dish} from "../dishes/dish.model";
+import {CartService} from "../cart/cart.service";
+import {Subscription} from "rxjs";
 
 @Component({
-  selector: 'app-cart-item',
-  templateUrl: './cart-item.component.html',
-  styleUrls: ['./cart-item.component.css']
+    selector: 'app-cart-item',
+    templateUrl: './cart-item.component.html',
+    styleUrls: ['./cart-item.component.css']
 })
-export class CartItemComponent {
+export class CartItemComponent implements OnInit {
+    @Input() dish: Dish;
+    @Input() quantity: number;
+    @Output() reservedDishesNumberChanged = new EventEmitter<number>()
+    reservedDishesAmount: number;
 
+    constructor(private cartService: CartService) {
+    }
+
+    ngOnInit(): void {
+        this.reservedDishesAmount = this.cartService.getReservedDishes(this.dish);
+    }
+
+    onAddDish() {
+        if (this.reservedDishesAmount < this.dish.maxDishAmount) {
+            this.reservedDishesAmount += 1;
+            this.reservedDishesNumberChanged.emit(1);
+            this.cartService.addDish(this.dish);
+        }
+    }
+
+    onSubtractDish() {
+        if (this.reservedDishesAmount > 0) {
+            this.reservedDishesAmount -= 1;
+            this.reservedDishesNumberChanged.emit(-1);
+            this.cartService.subtractDish(this.dish);
+        }
+    }
 }
