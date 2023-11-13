c53db59207d2dc6453b5df1109e8c37604e9726f
krolikow <mkrolikowska01@poczta.fm>
2023-11-13 22:29:34 +0100

    ref: indentation

--- src/app/currency.service.ts
+++ src/app/currency.service.ts
@@ -2,18 +2,20 @@ import {Injectable} from '@angular/core';
 import {Dish} from "./dishes/dish.model";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class CurrencyService {
-  constructor() {}
 
-  calculatePrice(dish: Dish, currency: string) {
-    if (dish.currency === currency || !currency) return dish.price;
-    if (currency == '€') {
-      return 0.75 * dish.price;
-    } else if (currency == '$') {
-      return 0.87 * dish.price;
+    constructor() {
     }
-    return 1.1 * dish.price;
-  }
+
+    calculatePrice(dish: Dish, currency: string) {
+        if (dish.currency === currency || !currency) return dish.price;
+        if (currency == '€') {
+            return 0.75 * dish.price;
+        } else if (currency == '$') {
+            return 0.87 * dish.price;
+        }
+        return 1.1 * dish.price;
+    }
 }
