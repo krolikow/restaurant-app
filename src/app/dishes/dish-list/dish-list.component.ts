45f8e454c49ee09d6afbc1a7308a8ac8b1412f46
krolikow <mkrolikowska01@poczta.fm>
2023-11-18 23:23:43 +0100

    feat: loading-spinner

--- src/app/dishes/dish-list/dish-list.component.ts
+++ src/app/dishes/dish-list/dish-list.component.ts
@@ -8,52 +8,55 @@ import {CartService} from "../../cart/cart.service";
 
 
 @Component({
-    selector: 'app-dish-list',
-    templateUrl: './dish-list.component.html',
-    styleUrls: ['./dish-list.component.css']
+  selector: 'app-dish-list',
+  templateUrl: './dish-list.component.html',
+  styleUrls: ['./dish-list.component.css']
 })
 export class DishListComponent implements OnInit, OnDestroy {
-    totalReserved = 0;
-    subscription: Subscription;
-    dishes: Dish[];
-    cheapest: number;
-    mostExpensive: number;
-    currencies = ['$', '€']
-    selectedCurrency: string = '$';
-    fields = {}
+  totalReserved = 0;
+  subscription: Subscription;
+  dishes: Dish[];
+  cheapest: number;
+  mostExpensive: number;
+  currencies = ['$', '€']
+  selectedCurrency: string = '$';
+  isLoading = false;
+  fields = {}
 
-    constructor(private dishService: DishService,
-                private cartService: CartService,
-                private router: Router,
-                private route: ActivatedRoute) {
+  constructor(private dishService: DishService,
+              private cartService: CartService,
+              private router: Router,
+              private route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.isLoading = true;
+    this.subscription = this.dishService.getDishes().subscribe(
+      dishes => {
+        this.dishes = dishes;
+        this.cheapest = this.dishService.getCheapestDish(dishes).price;
+        this.mostExpensive = this.dishService.getMostExpensiveDish(dishes).price;
+        this.totalReserved = this.cartService.getTotalReservedDishes();
+        this.isLoading = false;
+      });
+  }
+
+
+  onReservedDishesChanged(factor: number) {
+    this.totalReserved += factor;
+  }
+
+  updateFilters($event: any) {
+    this.fields = $event;
+  }
+
+  onMenuNavigate() {
+    this.router.navigate(['../', 'cart'], {relativeTo: this.route})
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
     }
-
-    ngOnInit(): void {
-        this.subscription = this.dishService.getDishes().subscribe(
-            dishes => {
-                this.dishes = dishes;
-                this.cheapest = this.dishService.getCheapestDish(dishes).price;
-                this.mostExpensive = this.dishService.getMostExpensiveDish(dishes).price;
-                this.totalReserved = this.cartService.getTotalReservedDishes();
-            });
-    }
-
-
-    onReservedDishesChanged(factor: number) {
-        this.totalReserved += factor;
-    }
-
-    updateFilters($event: any) {
-        this.fields = $event;
-    }
-
-    onMenuNavigate() {
-        this.router.navigate(['../', 'cart'], {relativeTo: this.route})
-    }
-
-    ngOnDestroy(): void {
-        if (this.subscription) {
-            this.subscription.unsubscribe();
-        }
-    }
+  }
 }
