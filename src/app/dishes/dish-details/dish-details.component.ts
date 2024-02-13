1dc01de338a62b7283cf3dd35ede2b4c6284ad27
krolikow <mkrolikowska01@poczta.fm>
2024-02-13 22:03:08 +0100

    fix: subscription bug fixed

--- src/app/dishes/dish-details/dish-details.component.ts
+++ src/app/dishes/dish-details/dish-details.component.ts
@@ -9,90 +9,88 @@ import {ActivatedRoute, Params} from "@angular/router";
 import {CurrencyService} from "../../currency.service";
 
 @Component({
-    selector: 'app-dish-details',
-    templateUrl: './dish-details.component.html',
-    styleUrls: ['./dish-details.component.css']
+  selector: 'app-dish-details',
+  templateUrl: './dish-details.component.html',
+  styleUrls: ['./dish-details.component.css']
 })
 export class DishDetailsComponent implements OnInit, OnDestroy {
-    dish: Dish;
-    selectedCurrency: string;
-    @Output() reservedDishesNumberChanged = new EventEmitter<number>()
-    reservedDishesAmount = 0;
-    dangerousDishAmount = 3;
-    subscription!: Subscription;
-    rate: number = 0;
-    calculatedPrice: number;
-    @ViewChild('addReviewModal') public addReviewModal: ModalDirective;
-    @ViewChild('reviewsModal') public reviewsModal: ModalDirective;
-    id: string;
+  dish: Dish;
+  selectedCurrency: string;
+  @Output() reservedDishesNumberChanged = new EventEmitter<number>()
+  reservedDishesAmount = 0;
+  dangerousDishAmount = 3;
+  dishSubscription!: Subscription;
+  reviewSubscription!: Subscription;
+  rate: number = 0;
+  calculatedPrice: number;
+  @ViewChild('addReviewModal') public addReviewModal: ModalDirective;
+  @ViewChild('reviewsModal') public reviewsModal: ModalDirective;
+  id: string;
 
-    constructor(private dishService: DishService,
-                private reviewService: ReviewService,
-                private cartService: CartService,
-                private currencyService: CurrencyService,
-                private route: ActivatedRoute) {
+  constructor(private dishService: DishService,
+              private reviewService: ReviewService,
+              private cartService: CartService,
+              private currencyService: CurrencyService,
+              private route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.selectedCurrency = this.route.snapshot.queryParams['currency'];
+    this.dishSubscription = this.dishService.getDish(this.id).subscribe(dish => {
+      this.dish = dish;
+      this.calculatedPrice = this.currencyService.calculatePrice(this.dish, this.selectedCurrency);
+      this.dish.rate = this.dishService.calculateRate(this.dish)
+    });
+
+    this.reviewSubscription = this.reviewService.reviewAdded.subscribe(
+      () => {
+        this.hideAddReview();
+        if (this.reviewsModal) this.hideReviews();
+      }
+    )
+  }
+
+  onAddDish() {
+    if (this.reservedDishesAmount < this.dish.quantity) {
+      this.reservedDishesAmount += 1;
+      this.reservedDishesNumberChanged.emit(1);
+      this.cartService.addDish(this.dish);
     }
+  }
 
-    ngOnInit(): void {
-        this.route.params
-            .pipe(switchMap((params: Params) => {
-                console.log(params)
-                this.id = params['id'];
-                return this.route.queryParams
-
-            }), switchMap(queryParams => {
-                this.selectedCurrency = queryParams['currency'];
-                return this.dishService.getDish(this.id);
-            })).subscribe(dish => {
-            this.dish = dish;
-            this.calculatedPrice = this.currencyService.calculatePrice(this.dish, this.selectedCurrency);
-            this.dish.rate = this.dishService.calculateRate(this.dish)
-            console.log('dish ', this.dish)
-            });
-
-        this.subscription = this.reviewService.reviewAdded.subscribe(
-            () => {
-                this.hideAddReview();
-                if (this.reviewsModal) this.hideReviews();
-            }
-        )
+  onSubtractDish() {
+    if (this.reservedDishesAmount > 0) {
+      this.reservedDishesAmount -= 1;
+      this.reservedDishesNumberChanged.emit(-1);
+      this.cartService.subtractDish(this.dish);
     }
+  }
 
-    onAddDish() {
-        if (this.reservedDishesAmount < this.dish.quantity) {
-            this.reservedDishesAmount += 1;
-            this.reservedDishesNumberChanged.emit(1);
-            this.cartService.addDish(this.dish);
-        }
-    }
+  public showAddReview(): void {
+    this.addReviewModal.show();
+  }
 
-    onSubtractDish() {
-        if (this.reservedDishesAmount > 0) {
-            this.reservedDishesAmount -= 1;
-            this.reservedDishesNumberChanged.emit(-1);
-            this.cartService.subtractDish(this.dish);
-        }
-    }
+  public hideAddReview(): void {
+    this.addReviewModal.hide();
+  }
 
-    public showAddReview(): void {
-        this.addReviewModal.show();
-    }
+  public showReviews(): void {
+    this.reviewsModal.show();
+  }
 
-    public hideAddReview(): void {
-        this.addReviewModal.hide();
-    }
+  public hideReviews(): void {
+    this.reviewsModal.hide();
+  }
 
-    public showReviews(): void {
-        this.reviewsModal.show();
+  ngOnDestroy(): void {
+    if (this.dishSubscription) {
+      this.dishSubscription.unsubscribe();
     }
-
-    public hideReviews(): void {
-        this.reviewsModal.hide();
+    if (this.reviewSubscription) {
+      this.reviewSubscription.unsubscribe();
     }
-
-    ngOnDestroy(): void {
-        this.subscription.unsubscribe();
-    }
+  }
 }
 
 
